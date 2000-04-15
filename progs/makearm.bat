7faac079c4aab5ab45a5ffe979f18a55258a76f4
Randy Brukardt <randy@rrsoftware.com>
2000-04-15 21:58:25 +0200

    End of first day's work.

--- progs/makearm.bat
+++ progs/makearm.bat
@@ -1,1 +1,1 @@
-corder arm_formatter /t/z/w/k255/b'arm.bat'/l'ads'/l'a'/l'ada'/n'adb'/n'ada'/n'a'/c'/zt/t'/jb'/t/l'/q'/w'/pDEFAULT/r.\object/jx'armlkc'
+corder arm_formatter /t/z/w/k255/b'arm.bat'/l'ads'/n'adb'/n'ada'/n'a'/c'/zt/t'/jb'/t/l'/q'/w'/r.\object/jx'alkc arm_form'
